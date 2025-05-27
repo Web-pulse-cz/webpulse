23b80af92e60e6ec894f62dff26cbef561129178
martashanzl2000 <martin.hanzl@monstermedia.cz>
2025-05-27 17:14:00 +0200

    novelties admin update & config

--- server/config/filemanager.php
+++ server/config/filemanager.php
@@ -12,6 +12,12 @@ return [
         'user' => [
             ['format' => 'icon', 'width' => 32, 'height' => 32, 'keepAspectRatio' => true, 'path' => 'icon'],
             ['format' => 'thumb', 'width' => 190, 'height' => 190, 'keepAspectRatio' => true, 'path' => 'thumb'],
-        ]
+        ],
+
+        'novelty' => [
+            ['format' => 'small', 'width' => 200, 'height' => 200, 'keepAspectRatio' => true, 'path' => 'small'],
+            ['format' => 'medium', 'width' => 400, 'height' => 400, 'keepAspectRatio' => true, 'path' => 'medium'],
+            ['format' => 'large', 'width' => 800, 'height' => 800, 'keepAspectRatio' => true, 'path' => 'large'],
+        ],
     ]
 ];
