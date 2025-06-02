2274b6943bb79ae1a36fb5d94ca8794fb46037af
martashanzl2000 <martin.hanzl@monstermedia.cz>
2025-06-02 14:25:10 +0200

    home technologies minor update

--- client/components/Home/Technologies.vue
+++ client/components/Home/Technologies.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
 const { t } = useI18n();
+
+const technologies = [
+  { src: '/static/img/icon/technologies/laravel.svg', alt: 'Laravel' },
+  { src: '/static/img/icon/technologies/docker.svg', alt: 'Docker' },
+  { src: '/static/img/icon/technologies/aws.svg', alt: 'Amazon web services' },
+  { src: '/static/img/icon/technologies/nodejs.svg', alt: 'Node.js' },
+  { src: '/static/img/icon/technologies/npm.svg', alt: 'Npm' },
+  { src: '/static/img/icon/technologies/php.svg', alt: 'PHP' },
+  { src: '/static/img/icon/technologies/nuxt.svg', alt: 'Nuxt.js' },
+  { src: '/static/img/icon/technologies/js.svg', alt: 'JavaScript' },
+  { src: '/static/img/icon/technologies/github.svg', alt: 'Github' },
+  { src: '/static/img/icon/technologies/figma.svg', alt: 'Figma' },
+  { src: '/static/img/icon/technologies/mysql.svg', alt: 'MySQL' },
+];
 </script>
 
 <template>
@@ -8,41 +22,26 @@ const { t } = useI18n();
       {{ t('home.technologiesTitle') }}
     </BasePropsHeading>
     <div class="flex flex-wrap justify-center gap-x-6">
-      <NuxtImg
-        src="/static/img/icon/technologies/laravel.svg"
-        alt="Laravel"
-        class="h-auto w-[96px]"
-      />
-      <NuxtImg
-        src="/static/img/icon/technologies/docker.svg"
-        alt="Docker"
-        class="h-auto w-[96px]"
-      />
-      <NuxtImg
-        src="/static/img/icon/technologies/aws.svg"
-        alt="Amazon web services"
-        class="h-auto w-[96px]"
-      />
-      <NuxtImg
-        src="/static/img/icon/technologies/nodejs.svg"
-        alt="Node.js"
-        class="h-auto w-[96px]"
-      />
-      <NuxtImg src="/static/img/icon/technologies/npm.svg" alt="Npm" class="h-auto w-[96px]" />
-      <NuxtImg src="/static/img/icon/technologies/php.svg" alt="PHP" class="h-auto w-[96px]" />
-      <NuxtImg src="/static/img/icon/technologies/nuxt.svg" alt="Nuxt.js" class="h-auto w-[96px]" />
-      <NuxtImg
-        src="/static/img/icon/technologies/js.svg"
-        alt="JavaScript"
-        class="h-auto w-[96px]"
-      />
-      <NuxtImg
-        src="/static/img/icon/technologies/github.svg"
-        alt="Github"
-        class="h-auto w-[96px]"
-      />
-      <NuxtImg src="/static/img/icon/technologies/figma.svg" alt="Figma" class="h-auto w-[96px]" />
-      <NuxtImg src="/static/img/icon/technologies/mysql.svg" alt="Mysql" class="h-auto w-[96px]" />
+      <div v-for="(tech, index) in technologies" :key="index" class="img-container">
+        <NuxtImg :src="tech.src" :alt="tech.alt" :lazyload="true" format="webp" />
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.img-container {
+  width: 96px;
+  height: 96px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.img-container img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+}
+</style>
