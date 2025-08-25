aa9286c4d233de5c84766e56f8e9da4b9e92074a
Martin Hanzl <martas.hanzl@email.cz>
2025-08-25 15:34:20 +0200

    fed reward client carousel update
    fed reward client frontend update

--- fed/app/layouts/default.vue
+++ fed/app/layouts/default.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+  <div class="mx-auto max-w-7xl px-4 py-6 py-8 sm:px-6 lg:px-8">
     <slot />
   </div>
 </template>
