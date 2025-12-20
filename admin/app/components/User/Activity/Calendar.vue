83b3ec32c0d558d28e9dfd0dfc76b034c04e0c61
martinhanzl <martas.hanzl@email.cz>
2025-12-20 22:25:20 +0100

    Admnin acitivity calendar update

--- admin/app/components/User/Activity/Calendar.vue
+++ admin/app/components/User/Activity/Calendar.vue
@@ -212,7 +212,7 @@ const activityNameById = computed(() => {
             @click="emit('update-item', activityItem)"
           >
             <div
-              v-if="[6, 8, 11, 12].includes(activityItem.activity.id) && !activityItem.completed"
+              v-if="[6, 8, 11, 12, 33].includes(activityItem.activity.id) && !activityItem.completed"
               class="text-md col-span-1 flex items-center justify-center text-xs font-semibold text-dangerLight"
             >
               <div class="flex h-8 w-8 items-center justify-center border-b border-r border-danger">
@@ -221,7 +221,7 @@ const activityNameById = computed(() => {
             </div>
             <div
               v-else-if="
-                [6, 8, 11, 12].includes(activityItem.activity.id) && activityItem.completed
+                [6, 8, 11, 12, 33].includes(activityItem.activity.id) && activityItem.completed
               "
               class="text-md col-span-1 flex items-center justify-center text-xs font-semibold text-grayLight"
             >
