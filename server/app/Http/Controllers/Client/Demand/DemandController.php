ba6552c8abae3c522b8027ea710ade6e3c5ed10b
martinhanzl <martas.hanzl@email.cz>
2026-01-16 21:34:21 +0100

    Client demand controller rules update

--- server/app/Http/Controllers/Client/Demand/DemandController.php
+++ server/app/Http/Controllers/Client/Demand/DemandController.php
@@ -21,7 +21,8 @@ class DemandController extends Controller
         $validator = Validator::make($request->all(), [
             'fullname' => 'required|string|max:255',
             'email' => 'required|email|max:255',
-            'phone' => 'required|string|max:15',
+            'phone' => 'required|string|max:17',
+            'message' => 'required|string',
         ]);
 
         if ($validator->fails()) {
