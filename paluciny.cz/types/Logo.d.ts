4967e5201b9057d8cdb284b11f12cbf541c9821a
martinhanzl <martas.hanzl@email.cz>
2026-04-28 12:16:00 +0200

    Major sitest update

--- paluciny.cz/types/Logo.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Logo {
-  id: number;
-  name: string;
-  url: string;
-  image: string;
-}
--- paluciny.cz/types/PostCategory.d.ts
+++
@@ -1,9 +0,0 @@
-export interface PostCategory {
-  id: number;
-  image: string;
-  name: string;
-  slug: string;
-  perex: string;
-  meta_title: string;
-  meta_description: string;
-}
